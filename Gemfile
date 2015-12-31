665e0995bad5c7926e6a96f7729fee67f7c2d4cf
june29 <june29.jp@gmail.com>
2015-12-31 19:01:59 +0900

    Implement basic logic

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
 ruby '2.3.0'
-gem 'twitter'
+gem 'twitter','= 5.14.0'
 gem 'dotenv'
 gem 'pry'
--- collector.rb
+++ collector.rb
@@ -1,10 +1,26 @@
+require 'logger'
 require 'bundler'
 Bundler.require(:default)
 Dotenv.load
+
+LOGGER = Logger.new(STDOUT)
+LOGGER.formatter = proc { |severity, datetime, progname, msg| "#{msg}\n" }
+STDOUT.sync = true
 
 client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
   config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+end
+
+client.filter(track: "NHK紅白") do |object|
+  begin
+    next unless object.is_a?(Twitter::Tweet)
+
+    tweet = object
+    LOGGER.info tweet.to_h
+  rescue => error
+    LOGGER.info error
+  end
 end

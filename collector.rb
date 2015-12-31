78b1521c0017677fc291c27b3a3d7db06c4fa56a
june29 <june29.jp@gmail.com>
2015-12-31 19:15:52 +0900

    Save Tweet data as file

--- collector.rb
+++ collector.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'logger'
 require 'bundler'
 Bundler.require(:default)
@@ -19,7 +20,14 @@ client.filter(track: "NHK紅白") do |object|
     next unless object.is_a?(Twitter::Tweet)
 
     tweet = object
-    LOGGER.info tweet.to_h
+
+    id = tweet.id
+    dirname = File.join(__dir__, '2015', 'tweets', ((id / 10000).to_i * 10000).to_s)
+
+    FileUtils.mkdir(dirname) unless File.exist?(dirname)
+    File.write(File.join(dirname, "#{id}.json"), tweet.to_hash.to_json)
+
+    puts "[#{tweet.id}][#{(tweet.created_at + 60 * 60 * 9).strftime('%H:%M:%S')}] @#{tweet.user.screen_name}: #{tweet.text.gsub(/\n/, '')}"
   rescue => error
     LOGGER.info error
   end
